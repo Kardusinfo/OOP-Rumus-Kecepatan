2537446a4d0d31b363aaaa7015e5c8d7685b3bd1
candraprasetya <candraramadhanp@gmail.com>
2020-04-04 18:54:21 +0800

    Initial commit

---
+++ Hitung.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPDasar
+{
+    class Hitung
+    {
+
+        private double jarak;
+        private double waktu;
+        private double kecepatan;
+
+        public double getJarak()
+        {
+            return jarak;
+        }
+
+        public void setJarak(double jaraknya)
+        {
+            jarak = jaraknya;
+        }
+        public double getWaktu()
+        {
+            return waktu;
+        }
+
+        public void setWaktu(double waktunya)
+        {
+            waktu = waktunya;
+        }
+
+        public void cariKecepatan()
+        {
+            kecepatan = jarak / waktu;
+            Console.WriteLine($"Hasilnya adalah {kecepatan} m/s");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOPDasar
+{
+    class Program
+    {
+
+/*     Mainan Motor – motoran Kak Candra menempuh jarak 100 meter dengan waktu 25 detik.
+       Berapakah kecepatan motor-motoran Kak Candra?
+*/
+        static void Main(string[] args)
+        {
+            //proses inisialiasi
+            var hitung = new Hitung();
+
+            //proses input jarak
+            Console.WriteLine("Masukkan jarak dalam satuan meter");
+            double jarakKu = Convert.ToDouble(Console.ReadLine());
+            hitung.setJarak(jarakKu);
+
+            //proses input waktu
+            Console.WriteLine("Masukkan waktu dalam satuan detik");
+            double waktuKu = Convert.ToDouble(Console.ReadLine());
+            hitung.setWaktu(waktuKu);
+
+            //proses menampilkan data waktu dan jarak
+            Console.WriteLine($"Jarak adalah {hitung.getJarak()} m");
+            Console.WriteLine($"Waktu adalah {hitung.getWaktu()} s");
+
+            //proses perhitungan waktu
+            hitung.cariKecepatan();
+
+           
+        }
+    }
+}
